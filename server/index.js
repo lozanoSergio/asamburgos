d1c40dbf64c9173a2aa147448ba17bcbbb472698
Sergio <sergylozanodelrio@gmail.com>
2019-04-23 13:27:18 +0200

    Mayor server port error fixed

--- server/index.js
+++ server/index.js
@@ -56,9 +56,11 @@ app
       }
     });
 
-    server.listen(3000, err => {
+    const PORT = process.env.PORT || 3000;
+
+    server.use(handle).listen(PORT, err => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+      console.log("> Ready on port " + PORT);
     });
   })
   .catch(ex => {
