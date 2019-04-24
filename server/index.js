f240b3b43bdccce72fd2bbeb7264248440992b14
Sergio <sergylozanodelrio@gmail.com>
2019-04-24 21:24:12 +0200

    request headers

--- server/index.js
+++ server/index.js
@@ -43,10 +43,11 @@ app
       return res.status(200).sendFile("robots.txt", robotsOptions);
     });
 
-    server.get("*", (req, res) => {
-      if (!dev && !req.secure) {
-        res.redirect('https://' + req.headers.host + req.url);
+    server.get("*", (req, res, next) => {
+      if(!dev && req.headers['x-forwarded-proto']!='https') {
+        res.redirect('https://asam-burgos.herokuapp.com' + req.url);
       }
+      next();
       return handle(req, res);
     });
 
