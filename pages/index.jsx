2b5d94663d0f41565c4ca83a1850d70435a23343
Sergio <sergylozanodelrio@gmail.com>
2019-04-25 14:24:53 +0200

    added chart in index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import ProfileTable from "../components/tables/ProfileTable";
-import withAuth from '../components/hoc/withAuth';
+import LineChart from "../components/charts/LineChart";
+import withAuth from "../components/hoc/withAuth";
 import { getUserProfiles } from "../actions";
-
 
 class Index extends React.Component {
   static async getInitialProps({ req }) {
     let profiles = [];
+    let dates = [];
 
     try {
       profiles = await getUserProfiles(req);
     } catch (err) {
       console.log(err);
     }
-    return { profiles };
+
+    if (profiles) {
+      profiles.filter(profile => dates.push(profile.createdAt));
+    }
+
+    return { profiles, dates };
   }
   render() {
-    const {profiles} = this.props;
+    const { profiles, dates } = this.props;
     return (
       <BaseLayout>
+        <LineChart dates={dates} />
         <ProfileTable profiles={profiles} />
       </BaseLayout>
     );
   }
 }
 
-export default (Index);
+export default Index;
